0edd8e4b67edd09287c5b93805c31e0fb3f6ee14
SUHAS <suhasgs1113@gmail.com>
2026-03-03 13:38:13 +0530

    commit by suhas

---
+++ bubble.java
@@ -0,0 +1,8 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
