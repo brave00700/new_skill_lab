f4977085d05c21d85085c42e596eb246375aac1a
PramathHegde <pramathhegde76@gmail.com>
2026-03-03 13:41:09 +0530

    Commit by pranav

--- bubble.java
+++ bubble.java
@@ -6,3 +6,14 @@ public class BubbleSort {
 
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
+
+	 for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
